8f854894cb94993c9d78de9080af2d5d2730c554
krkrkrr <67230121+krkrkrr@users.noreply.github.com>
2024-11-05 01:35:34 +0900

    [update] Version 1.1
    Countdown to next holiday or business day.

--- src/script.js
+++ src/script.js
@@ -1,20 +1,101 @@
+class Holiday {
+    constructor() {
+        this.holidays = {};
+    }
+
+    async fetchHolidays() {
+        try{
+            const response = await fetch('https://holidays-jp.github.io/api/v1/date.json');
+            const json = await response.json();
+            this.holidays = json;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    isHoliday(date) {
+        const tempDate = new Date(date);
+        if(tempDate.getDay() == 0 || tempDate.getDay() == 6) {
+            return true;
+        }
+        const dateString = tempDate.toLocaleDateString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/\//g, '-');
+        return dateString in this.holidays;
+    }
+
+    isHolidayTime(date) {
+        const tempDate = new Date(date);
+        if(tempDate.getHours() >= 17) {
+            tempDate.setDate(tempDate.getDate() + 1);
+        } else if(tempDate.getHours() < 6) {
+            tempDate.setDate(tempDate.getDate() - 1);
+        }
+        if(tempDate.getDay() == 0 || tempDate.getDay() == 6) {
+            return true;
+        }
+        const dateString = tempDate.toLocaleDateString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/\//g, '-');
+        return dateString in this.holidays;
+    }
+    
+    getNextHoliday() {
+        let nextDate = new Date();
+
+        for(let i = 0; i < 14; i++) {
+            if(this.isHoliday(nextDate)) {
+                break;
+            }
+            nextDate.setDate(nextDate.getDate() + 1);
+        }
+        nextDate.setHours(17);
+        nextDate.setMinutes(0);
+        nextDate.setSeconds(0);
+        nextDate.setMilliseconds(0);
+        return nextDate;
+    }
+    
+    getNextBusinessday() {
+        let nextDate = new Date();
+        for(let i = 0; i < 14; i++) {
+            if(!this.isHoliday(nextDate)) {
+                break;
+            }
+            nextDate.setDate(nextDate.getDate() + 1);
+        }
+        
+        nextDate.setHours(6);
+        nextDate.setMinutes(0);
+        nextDate.setSeconds(0);
+        nextDate.setMilliseconds(0);
+        return nextDate;
+    }
+}
+
 window.onload = function() {
-    const targetDate = new Date('2024-10-01T00:00:00');
+    const holiday = new Holiday();
+    holiday.fetchHolidays();
     const timerElement = document.getElementById('timer');
 
     const interval = setInterval(() => {
-        const diffDate = targetDate - new Date();
-        if (diffDate <= 0) {
-            clearInterval(interval);
-            timerElement.textContent = "00:00:00:00";
-            alert("無職卒業おめでとう！！");
+        if(holiday.holidays.length === 0) {
             return;
         }
+        let targetDate;
+        if (holiday.isHolidayTime(new Date())) {
+            targetDate = holiday.getNextBusinessday();
+        } else {
+            targetDate = holiday.getNextHoliday();
+        }
+        const diffDate = Math.abs(targetDate - new Date());
 
         const days = Math.floor(diffDate / (1000 * 60 * 60 * 24));
         const hrs = Math.floor((diffDate / (1000 * 60 * 60)) % 24);
         const mins = Math.floor((diffDate / (1000 * 60)) % 60);
         const secs = Math.floor((diffDate / 1000) % 60);
+
+        if (holiday.isHolidayTime(new Date())) {
+            document.getElementById('timer-decralation').textContent = '出勤まで残り:';
+        } else {
+            document.getElementById('timer-decralation').textContent = '休みまで残り:';
+        }
 
         timerElement.textContent = 
             `${String(days).padStart(2, '0')}:${String(hrs).padStart(2, '0')}:${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
@@ -22,3 +103,4 @@ window.onload = function() {
 
 
 }
+
